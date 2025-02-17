ea715fe941de07a9e90e95c6695af643cd4ce081
AbhishekKolge <abhishekkolge96@gmail.com>
2025-02-17 23:48:51 +0530

    add dotenv for environment variable management and configuration validation
    Signed-off-by: AbhishekKolge <abhishekkolge96@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+import { config } from './config';
 import { databaseConnection } from './setup-database';
 import { AppServer } from './setup-server';
 import express, { type Express } from 'express';
 
 class Application {
   public async initialize(): Promise<void> {
+    this.loanConfig();
     await databaseConnection();
     const app: Express = express();
     const server: AppServer = new AppServer(app);
     server.start();
+  }
+
+  private loanConfig(): void {
+    config.validateConfig();
   }
 }
 
---
+++ src/config.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class Config {
+  public DATABASE_URL?: string;
+  public JWT_TOKEN?: string;
+  public NODE_ENV?: string;
+  public SECRET_KEY_ONE?: string;
+  public SECRET_KEY_TWO?: string;
+  public CLIENT_URL?: string;
+
+  constructor() {
+    this.DATABASE_URL = process.env.DATABASE_URL;
+    this.JWT_TOKEN = process.env.JWT_TOKEN;
+    this.NODE_ENV = process.env.NODE_ENV;
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE;
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO;
+    this.CLIENT_URL = process.env.CLIENT_URL;
+  }
+
+  public validateConfig(): void {
+    for (const [key, value] of Object.entries(this)) {
+      if (!value) {
+        throw new Error(`Configuration ${key} is missing.`);
+      }
+    }
+  }
+}
+
+export const config: Config = new Config();
