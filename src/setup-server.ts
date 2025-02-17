849a385483de647fee4175e45b557a5e7323b49e
Abhishek Kolge <abhishekkolge96@gmail.com>
2025-02-17 19:51:43 +0530

    add security and standard middleware to server setup
    Signed-off-by: Abhishek Kolge <abhishekkolge96@gmail.com>

--- src/setup-server.ts
+++ src/setup-server.ts
@@ -1,4 +1,9 @@
-import type { Application } from "express";
+import compression from "compression";
+import cookieSession from "cookie-session";
+import cors from "cors";
+import { json, urlencoded, type Application } from "express";
+import helmet from "helmet";
+import hpp from "hpp";
 import type { Server } from "http";
 
 export class AppServer {
@@ -16,9 +21,32 @@ export class AppServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: [],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
 
   private routesMiddleware(app: Application): void {}
 
