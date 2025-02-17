6c4872240cc7383fcd3bb0a4d44dda260a3becfb
Abhishek Kolge <abhishekkolge96@gmail.com>
2025-02-17 19:22:34 +0530

    server class setup
    Signed-off-by: Abhishek Kolge <abhishekkolge96@gmail.com>

---
+++ src/setup-server.ts
@@ -0,0 +1,32 @@
+import type { Application } from "express";
+import type { Server } from "http";
+
+export class AppServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {}
+
+  private standardMiddleware(app: Application): void {}
+
+  private routesMiddleware(app: Application): void {}
+
+  private globalErrorHandler(app: Application): void {}
+
+  private startServer(app: Application): void {}
+
+  private createSocketIo(httpServer: Server): void {}
+
+  private startHttpServer(httpServer: Server): void {}
+}
